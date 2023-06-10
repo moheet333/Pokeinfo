800bcea3f2bf05223e278552c8af877b58829bc1
moheet333 <moheetshendarkar@gmail.com>
2023-06-10 20:44:05 +0530

    V1 done

---
+++ src/pages/[pokemon].tsx
@@ -0,0 +1,74 @@
+import { useRouter } from "next/router";
+import usePokemon from "@/hooks/usePokemon";
+import Head from "next/head";
+import Link from "next/link";
+import { Button, Form, Spinner } from "react-bootstrap";
+import Image from "next/image";
+import { FormEvent } from "react";
+import * as PokemonApi from "@/network/pokemon-api";
+
+export default function PokemonDetailsPage() {
+  const router = useRouter();
+  const pokemonName = router.query.pokemon?.toString() || "";
+  const { pokemon, pokemonLoading, mutatePokemon } = usePokemon(pokemonName);
+
+  async function handleSubmitNickname(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const nickname = formData.get("nickname")?.toString().trim();
+    if (!pokemon || !nickname) {
+      return;
+    }
+    const update = await PokemonApi.setNickName(pokemon, nickname);
+    mutatePokemon(update, { revalidate: false });
+  }
+
+  return (
+    <>
+      <Head>{pokemon && <title>{`${pokemon.name} - Pokeinfo`}</title>}</Head>
+      <div className="d-flex flex-column align-items-center">
+        <p>
+          <Link href="/" className="link-light">
+            ← Pokeinfo
+          </Link>
+        </p>
+        {pokemonLoading && <Spinner animation="grow" />}
+        {pokemon === null && <p>Pokemon not found</p>}
+        {pokemon && (
+          <>
+            <h1 className="text-center text-capitalize">{pokemon.name}</h1>
+            <Image
+              src={pokemon.sprites.other["official-artwork"].front_default}
+              alt={"Pokemon: " + pokemon.name}
+              width={400}
+              height={400}
+            />
+            <div className="d-inline-block mt-2">
+              <div>
+                <strong>Types:</strong>{" "}
+                {pokemon.types
+                  .map((type) => {
+                    return type.type.name;
+                  })
+                  .join(" & ")}
+              </div>
+              <div>
+                <strong>Height:</strong> {pokemon.height * 10} cm
+              </div>
+              <div>
+                <strong>Weight:</strong> {pokemon.weight / 10} kg
+              </div>
+            </div>
+            <Form onSubmit={handleSubmitNickname} className="mt-4">
+              <Form.Group controlId="pokemon-nickname-input" className="mb-3">
+                <Form.Label>Give this Pokemon a nickname</Form.Label>
+                <Form.Control name="nickname" placeholder="E.g. John" />
+              </Form.Group>
+              <Button type="submit">Set nickname</Button>
+            </Form>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Anton } from "next/font/google";
+import { Container } from "react-bootstrap";
+import Head from "next/head";
+
+const anton = Anton({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Pokeinfo</title>
+        <meta
+          name="description"
+          content="Get Pokemon information using client side rendering"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={anton.className}>
+        <main>
+          <Container className="py-4">
+            <Component {...pageProps} />
+          </Container>
+        </main>
+      </div>
+    </>
+  );
 }
